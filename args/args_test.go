9e82830a0ecdca9e50c8ae25be36186f1e5f99ed
inflamously <david.dembinski.1996@gmail.com>
2023-08-20 20:02:49 +0200

    feat: extend tests

--- args/args_test.go
+++ args/args_test.go
@@ -2,10 +2,11 @@ package args
 
 import (
 	"fmt"
+	"github.com/inflamously/goextensions/tests"
 	"testing"
 )
 
-func TestCommand_Parse(t *testing.T) {
+func TestCommand_ParseTooManyArguments(t *testing.T) {
 	args := []string{
 		"tags",
 		"random",
@@ -15,7 +16,7 @@ func TestCommand_Parse(t *testing.T) {
 		"123",
 	}
 
-	command := NewCommand(
+	command := CreateCommand(
 		&SimpleCommand{
 			Name: "tags",
 			Function: func(args []SimpleArgument, options []SimpleOption) {
@@ -35,7 +36,42 @@ func TestCommand_Parse(t *testing.T) {
 			},
 		}).
 		Argument(&SimpleArgument{}).
-		Root()
+		RootCommand()
+
+	defer tests.StopPanic("Command did not panic due to too many arguments")
+
+	command.Parse(args)
+}
+
+func TestCommand_ParseSubcommand(t *testing.T) {
+	args := []string{
+		"tags",
+		"random",
+		"10",
+	}
+
+	command := CreateCommand(
+		&SimpleCommand{
+			Name: "tags",
+			Function: func(args []SimpleArgument, options []SimpleOption) {
+				fmt.Printf("Running 'tags' command with args: %s \n", args)
+			},
+		}).
+		Option(
+			"--test",
+			[]*SimpleArgument{
+				{}, {},
+			},
+		).
+		Subcommand(&SimpleCommand{
+			Name: "random",
+			Function: func(args []SimpleArgument, options []SimpleOption) {
+				fmt.Printf("Running 'random' command with args: %s \n", args)
+			},
+		}).
+		Argument(&SimpleArgument{}).
+		RootCommand()
+
 	command.Parse(args)
 }
 
@@ -49,7 +85,7 @@ func TestCommand_ParseTags(t *testing.T) {
 		"123",
 	}
 
-	command := NewCommand(
+	command := CreateCommand(
 		&SimpleCommand{
 			Name: "tags",
 			Function: func(args []SimpleArgument, options []SimpleOption) {
@@ -67,7 +103,7 @@ func TestCommand_ParseTags(t *testing.T) {
 				fmt.Printf("Running 'random' command with args: %s \n", args)
 			},
 		}).
-		Root()
+		RootCommand()
 
 	for i := len(args); i > 0; i-- {
 		command.Parse(args[:i])
@@ -82,7 +118,7 @@ func TestCommand_ParseOptions(t *testing.T) {
 		"123",
 	}
 
-	command := NewCommand(
+	command := CreateCommand(
 		&SimpleCommand{
 			Name: "tags",
 		}).
@@ -105,7 +141,38 @@ func TestCommand_ParseOptions(t *testing.T) {
 				{},
 			},
 		).
-		Root()
+		RootCommand()
+
+	command.Parse(args)
+}
+
+func TestCommand_Argument(t *testing.T) {
+	args := []string{
+		"tags",
+		"random",
+	}
+
+	command := CreateCommand(
+		&SimpleCommand{
+			Name: "tags",
+		},
+	).Option(
+		"--test",
+		[]*SimpleArgument{
+			{}, {},
+		},
+	).Option(
+		"--x",
+		nil,
+	).Subcommand(
+		&SimpleCommand{
+			Name: "random",
+		},
+	).Argument(
+		&SimpleArgument{},
+	).RootCommand()
+
+	defer tests.StopPanic("Command did not panic due to missing arguments")
 
 	command.Parse(args)
 }
