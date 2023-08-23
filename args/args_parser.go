cbb733fcd4d7d31f63b508bd11b8167d9b76925f
inflamously <david.dembinski.1996@gmail.com>
2023-08-23 21:27:59 +0200

    fix: incase of failed command parseSubcommand not returned that it parsed feat: return state of parse

--- args/args_parser.go
+++ args/args_parser.go
@@ -5,23 +5,26 @@ import (
 	"log"
 )
 
+/*
+Checks subcommands and if matches, parses args further.
+*/
 func (c *SimpleCommand) parseSubcommand(mutArgs []string) bool {
 	for _, command := range c.subcommands {
 		if len(mutArgs) > 0 && command.Name == mutArgs[0] {
-			if command.Parse(mutArgs) {
-				return true
-			}
+			command.Parse(mutArgs)
+			return true
 		}
 	}
 
 	return false
 }
 
-func (c *SimpleCommand) parseArguments(mutArgs []string) []string {
+func (c *SimpleCommand) parseArguments(mutArgs []string) ([]string, bool) {
 	if c.arguments != nil && len(c.arguments) > 0 {
 		argsCount := len(c.arguments)
 		if argsCount > len(mutArgs) {
-			log.Panicf("Command '%s' is missing '%d' arguments", c.Name, argsCount)
+			log.Printf("Command '%s' is missing '%d' arguments", c.Name, argsCount)
+			return mutArgs, false
 		}
 		parseArguments := mutArgs[:argsCount]
 		for argIndex, arg := range c.arguments {
@@ -29,10 +32,10 @@ func (c *SimpleCommand) parseArguments(mutArgs []string) []string {
 		}
 	}
 	mutArgs = mutArgs[len(c.arguments):]
-	return mutArgs
+	return mutArgs, true
 }
 
-func (c *SimpleCommand) parseOptions(mutArgs []string) []string {
+func (c *SimpleCommand) parseOptions(mutArgs []string) ([]string, bool) {
 	var removeIndexes []int
 	if c.options != nil {
 		for _, option := range c.options {
@@ -49,7 +52,8 @@ func (c *SimpleCommand) parseOptions(mutArgs []string) []string {
 							optionArg.Value = mutArgs[optionArgIndex]
 							removeIndexes = append(removeIndexes, optionArgIndex)
 						} else {
-							log.Panicf("Not enough arguments for option '%s' in command '%s'", option.Name, c.Name)
+							log.Printf("Not enough arguments for option '%s' in command '%s'", option.Name, c.Name)
+							return mutArgs, false
 						}
 					}
 				}
@@ -62,5 +66,5 @@ func (c *SimpleCommand) parseOptions(mutArgs []string) []string {
 		}
 	}
 
-	return mutArgs
+	return mutArgs, true
 }
